2f358967fe820e89f35123c887bdc4418952ca60
Matz Haugen <matz.haugen@cognite.com>
2021-02-24 10:35:25 +0100

    Add wait for rollout

--- contrib/load_test.sh
+++ contrib/load_test.sh
@@ -4,7 +4,6 @@ export OPENFAAS_URL=http://127.0.0.1:31112
 hey -z=5s -q 5 -c 2 -m POST -d=Test $OPENFAAS_URL/function/nodeinfo
 kubectl scale --replicas=0 deploy/nodeinfo -n openfaas-fn
 kubectl scale --replicas=10 deploy/nodeinfo -n openfaas-fn
-hey -z=5s -q 5 -c 2 -m POST -d=Test $OPENFAAS_URL/function/nodeinfo
-kubectl scale --replicas=0 deploy/nodeinfo -n openfaas-fn
+kubectl --context "kind-kind" rollout status deploy/nodeinfo -n openfaas-fn --timeout=4m
 hey -z=5s -q 5 -c 2 -m POST -d=Test $OPENFAAS_URL/function/nodeinfo
 kubectl scale --replicas=0 deploy/nodeinfo -n openfaas-fn
